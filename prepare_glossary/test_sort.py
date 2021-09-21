db3fe504b96ea27ecfbc2ae2e1da67240c58579b
Valeria.Shelgunova <Valeria.Shelgunova@techinput.ru>
2021-09-21 16:49:12 +0300

    test new git account

--- prepare_glossary/test_sort.py
+++ prepare_glossary/test_sort.py
@@ -1,6 +1,7 @@
 test_list = ["Abra", "abra", "Bot", "bot", "something", "Something", "abba", "", "", "", "", ]
 test_list.sort()
 print(test_list)
+
 test_list = ["Abra", "abra", "Bot", "bot", "something", "Something", "abba", "", "", "", "", ]
 test_list.sort(key=lambda x: x.casefold())
 print(test_list)
