0f53fc7b88e90ac1d8c1584fff3a1f6cd652a824
Valeria.Shelgunova <Valeria.Shelgunova@techinput.ru>
2021-10-20 18:01:18 +0300

    Added code for different colors

---
+++ tis_manager/_17_different_colors.py
@@ -0,0 +1,29 @@
+from docx import *
+from docx.enum.text import WD_COLOR_INDEX
+from typing import List
+
+colors_list = ["YELLOW", "BLACK", "BRIGHT_GREEN", "TEAL"]
+
+document = Document('./text/different_colors.docx')
+
+
+def return_pydocx_color(colors_list: List[str]):
+    wd_colors_list = [getattr(WD_COLOR_INDEX, color) for color in colors_list]
+    return wd_colors_list
+
+
+for t in document.tables:
+    for row in t.rows:
+        for cell in row.cells:
+            for p in cell.paragraphs:
+                for run in p.runs:
+                    if run.font.highlight_color in return_pydocx_color(colors_list):
+                        run.font.hidden = True
+
+for p in document.paragraphs:
+    for run in p.runs:
+        if run.font.highlight_color:
+            print(run.font.highlight_color)
+            run.font.hidden = True
+
+document.save('./text/done/different_colors.docx')
